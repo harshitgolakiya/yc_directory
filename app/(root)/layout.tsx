bb3a1ce271133b5970255b0bfa1eced994fce4ff
harshit golakiya <golakiyaharshit@gmail.com>
2025-08-03 18:30:27 +0400

    Updated layout and navbar; added fonts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Navbar from "@/app/components/navbar";
 
-export default function layout({children} : {children: React.ReactNode}){
+export default function layout({children} : Readonly<{children: React.ReactNode}>){
     return(
         <main className="font-work-sans">
             <Navbar />
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,34 +1,49 @@
-import { auth } from '@/auth'
-import { signOut } from 'next-auth/react'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const navbar = async () => {
-    const session = await auth();
+  const session = await auth();
   return (
-    <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-        <nav className='flex justify-between items-center'>
-            <Link href="/">
-            <Image src="/logo.png" alt="Logo" width={144} height={30} className='object-contain' />
-            </Link>
-            <div className='flex items-center gap-5'>
-                {session && session?.user ?(
-                    <>
-                        <Link href="/startup/create">
-                            <span>Create</span>
-                        </Link>
-                        <button onClick={signOut}>
-                            <span>logout</span>
-                        </button>
-                        <Link href={`/users`}>
-                        </Link>
-                    </>
-                )}
-            </div>
-        </nav>
-    </div>
-  )
-}
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans text-black">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30}></Image>
+        </Link>
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">sign out</button>
+              </form>
 
-export default navbar
+              <Link href={`/user/${session?.user?.id}`}>
+                <span> {session?.user?.name} </span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default navbar;
